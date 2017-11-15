7e7061ee31b55812a205d378eae158f0927ec3ad
Kowalski, Jacek <jacek.kowalski@dieboldnixdorf.com>
2017-11-15 20:01:05 +0100

    Event store sample1

---
+++ EventStoreSamples/EventStoreSamples/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace EventStoreSamples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPostMessage = new System.Windows.Forms.Button();
+            this.btnGetLast = new System.Windows.Forms.Button();
+            this.btnGetPrevious = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPostMessage
+            // 
+            this.btnPostMessage.Location = new System.Drawing.Point(12, 27);
+            this.btnPostMessage.Name = "btnPostMessage";
+            this.btnPostMessage.Size = new System.Drawing.Size(128, 23);
+            this.btnPostMessage.TabIndex = 0;
+            this.btnPostMessage.Text = "PostMessage";
+            this.btnPostMessage.UseVisualStyleBackColor = true;
+            this.btnPostMessage.Click += new System.EventHandler(this.btnPostMessage_Click);
+            // 
+            // btnGetLast
+            // 
+            this.btnGetLast.Location = new System.Drawing.Point(157, 27);
+            this.btnGetLast.Name = "btnGetLast";
+            this.btnGetLast.Size = new System.Drawing.Size(132, 23);
+            this.btnGetLast.TabIndex = 1;
+            this.btnGetLast.Text = "Get Last";
+            this.btnGetLast.UseVisualStyleBackColor = true;
+            this.btnGetLast.Click += new System.EventHandler(this.btnGetLast_Click);
+            // 
+            // btnGetPrevious
+            // 
+            this.btnGetPrevious.Location = new System.Drawing.Point(316, 27);
+            this.btnGetPrevious.Name = "btnGetPrevious";
+            this.btnGetPrevious.Size = new System.Drawing.Size(137, 23);
+            this.btnGetPrevious.TabIndex = 2;
+            this.btnGetPrevious.Text = "Get Previous";
+            this.btnGetPrevious.UseVisualStyleBackColor = true;
+            this.btnGetPrevious.Click += new System.EventHandler(this.btnGetPrevious_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 386);
+            this.Controls.Add(this.btnGetPrevious);
+            this.Controls.Add(this.btnGetLast);
+            this.Controls.Add(this.btnPostMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPostMessage;
+        private System.Windows.Forms.Button btnGetLast;
+        private System.Windows.Forms.Button btnGetPrevious;
+    }
+}
+
---
+++ EventStoreSamples/EventStoreSamples/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.ServiceModel.Syndication;
+
+namespace EventStoreSamples
+{
+    public partial class Form1 : Form
+    {
+        private Uri _last;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPostMessage_Click(object sender, EventArgs e)
+        {
+            var message = "[{'eventType':'MyFirstEvent', 'eventId':'"
+                          + Guid.NewGuid() + "', 'data':{'name':'hello world!', 'number':"
+                          + new Random().Next() + "}}]";
+            var request = WebRequest.Create("http://127.0.0.1:2113/streams/yourstream");
+            request.Method = "POST";
+            request.ContentType = "application/vnd.eventstore.events+json";
+            request.ContentLength = message.Length;
+            using (var sw = new StreamWriter(request.GetRequestStream()))
+            {
+                sw.Write(message);
+            }
+            using (var response = request.GetResponse())
+            {
+                response.Close();
+            }
+        }
+
+        private void btnGetLast_Click(object sender, EventArgs e)
+        {
+            this._last = GetLast(new Uri("http://127.0.0.1:2113/streams/yourstream"));
+        }
+
+        private static SyndicationLink GetNamedLink(IEnumerable<SyndicationLink> links, string name)
+        {
+            return links.FirstOrDefault(link => link.RelationshipType == name);
+        }
+
+        private static void ProcessItem(SyndicationItem item)
+        {
+            Console.WriteLine(item.Title.Text);
+            //get events
+            var request = (HttpWebRequest)WebRequest.Create(GetNamedLink(item.Links, "alternate").Uri);
+            request.Credentials = new NetworkCredential("admin", "changeit");
+            request.Accept = "application/json";
+            using (var response = request.GetResponse())
+            {
+                var streamReader = new StreamReader(response.GetResponseStream());
+                MessageBox.Show(streamReader.ReadToEnd());
+                //Console.WriteLine(streamReader.ReadToEnd());
+            }
+        }
+
+        private static Uri ReadPrevious(Uri uri)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Credentials = new NetworkCredential("admin", "changeit");
+            request.Accept = "application/atom+xml";
+            using (var response = request.GetResponse())
+            {
+                using (var xmlreader = XmlReader.Create(response.GetResponseStream()))
+                {
+                    var feed = SyndicationFeed.Load(xmlreader);
+                    foreach (var item in feed.Items.Reverse())
+                    {
+                        ProcessItem(item);
+                    }
+                    var prev = GetNamedLink(feed.Links, "previous");
+                    return prev == null ? uri : prev.Uri;
+                }
+            }
+        }
+
+        static Uri GetLast(Uri head)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(head);
+            request.Credentials = new NetworkCredential("admin", "changeit");
+            request.Accept = "application/atom+xml";
+            try
+            {
+                using (var response = (HttpWebResponse)request.GetResponse())
+                {
+                    if (response.StatusCode == HttpStatusCode.NotFound)
+                        return null;
+                    using (var xmlreader = XmlReader.Create(response.GetResponseStream()))
+                    {
+                        var feed = SyndicationFeed.Load(xmlreader);
+                        var last = GetNamedLink(feed.Links, "last");
+                        return (last != null) ? last.Uri : GetNamedLink(feed.Links, "self").Uri;
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                if (((HttpWebResponse)ex.Response).StatusCode == HttpStatusCode.NotFound) return null;
+                throw;
+            }
+        }
+
+        private void btnGetPrevious_Click(object sender, EventArgs e)
+        {
+            var current = ReadPrevious(this._last);
+        }
+    }
+}
